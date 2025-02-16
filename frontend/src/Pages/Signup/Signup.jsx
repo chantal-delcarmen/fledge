480bc6d1291a2f982db3ae4ca107dc899574b408
jjacob22 <160354928+jjacob22@users.noreply.github.com>
2025-02-16 10:42:46 -0700

    Update Signup.jsx

--- frontend/src/Pages/Signup/Signup.jsx
+++ frontend/src/Pages/Signup/Signup.jsx
@@ -5,25 +5,40 @@ import { useNavigate } from 'react-router-dom';
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPass, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
 
   const handleSignupSubmit = (e) => {
     e.preventDefault();
 
-    if (email === '' || password === '') {
+    if (email === '' || password === '' || username == '') {
       setErrorMessage('Both fields are required');
       return;
     }
+    else if (password != confirmPass) {
+      setErrorMessage('Passwords do not match!');
+      return;
+    }
 
-    // Mock sign-up validation
-    navigate('/explanation');  // Navigate to selection page
+    // Test sign up
+    navigate('/explanation');
   };
 
   return (
     <div>
       <h2>Sign Up</h2>
       <form onSubmit={handleSignupSubmit}>
+        <div>
+          <label>Username</label>
+          <input
+            type="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
         <div>
           <label>Email</label>
           <input
@@ -39,6 +54,15 @@ const Signup = () => {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            value={confirmPass}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
         </div>
