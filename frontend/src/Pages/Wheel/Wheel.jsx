e59e9378fb43cd4ac31312db530fac55d8d15070
jjacob22 <160354928+jjacob22@users.noreply.github.com>
2025-02-15 23:40:20 -0700

    Create Wheel.jsx

---
+++ frontend/src/Pages/Wheel/Wheel.jsx
@@ -0,0 +1,13 @@
+// Wheel.jsx
+import React from 'react';
+
+const Wheel = () => {
+  return (
+    <div>
+      <h2>Welcome Back!</h2>
+      <p>Spin the wheel?</p>
+    </div>
+  );
+};
+
+export default Wheel;
