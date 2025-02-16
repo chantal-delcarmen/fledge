1a2e3f4124efbfb6d2da7f83e98568f58ce48f22
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-02-16 11:42:07 -0700

    Connecting DoMorePage
    Fixing link

---
+++ frontend/src/Pages/DoMorePage/DoMorePage.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import TaskFlipper from "../../Components/TaskFlipper/TaskFlipper";
+import GenerateButton from "../../Components/Button/GenerateButton";
+// import './DoMore.scss'
+
+function DoMorePage() {
+    const task = [
+        'Have you set up a TFSA (tax free savings account)?',
+        'Schedule a doctor’s appointment.',
+        'Review your monthly expenses.',
+        'Organize your workspace.',
+        'Meal prep for the week.'
+    ];
+
+    const [currentTask, setCurrentTask] = useState(tasks[0]);
+
+    const getRandomTask = () => {
+        const randomTask = tasks [Math.floor(Math.random() * tasks.length)];
+        setCurrentTask(randomTask);
+    }
+
+    return(
+        <div className="do-more-page">
+            <h2>Do More</h2>
+            <p>Generate more fledges to do today:</p>
+            <TaskFlipper task={currentTask} />
+            <GenerateButton onClick={getRandomTask} />
+        </div>
+    );
+}
+
+export default DoMorePage;
